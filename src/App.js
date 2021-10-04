60766eefe6ee741808e644424512c097228cb030
drset <dr_set@hotmail.com>
2021-10-04 05:05:36 -0300

    Module 5.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+//https://github.com/academind/react-complete-guide-code/tree/05-rendering-lists-conditional-content
+
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from 'react';
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const expenses = [
-  { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-  { title: "New Desk", amount: 500.0, date: new Date(2021, 3, 15) },
-  { title: "Toilet Paper", amount: 14.55, date: new Date(2021, 2, 25) },
-  { title: "Ink", amount: 94.77, date: new Date(2021, 4, 1) },
+const DUMMY_EXPENSES = [
+  { id: 'e1', title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+  { id: 'e2', title: "New Desk", amount: 500.0, date: new Date(2021, 3, 15) },
+  { id: 'e3', title: "Toilet Paper", amount: 14.55, date: new Date(2021, 2, 25) },
+  { id: 'e4', title: "Ink", amount: 94.77, date: new Date(2021, 4, 1) },
 ];
 
 function App() {
 
+const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
 const addExpenseHandler = expense => {
-  console.log('In App.js');
-  console.log(expense);
+  setExpenses((prevExpenses) => {
+    return [expense, ... prevExpenses];
+  });
 };
 
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,17 @@
-import './ExpenseItem.css'
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
-import React, {useState} from 'react';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-  
-  const [title, setTitle] = useState(props.title);
-
-
-  const clickHandler = () => {
-      setTitle("Updated!");
-  };
-
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-        <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
     </Card>
   );
 }
